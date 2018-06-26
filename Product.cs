cfd8868081041118763db0c72bf7ed73b1a0d9ff
Colossal1999 <toanlvd00584@fpt.edu.vn>
2018-06-26 13:09:00 +0900

    Ass_C_Sharp

---
+++ Product.cs
@@ -0,0 +1,38 @@
+﻿namespace Ass_C_sharp
+{
+    public class Product
+    {
+        private string productId;
+        private string name;
+        private string price;
+
+        public Product()
+        {
+        }
+
+        public Product(string productId, string name, string price)
+        {
+            this.productId = productId;
+            this.name = name;
+            this.price = price;
+        }
+
+        public string ProductId
+        {
+            get => productId;
+            set => productId = value;
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public string Price
+        {
+            get => price;
+            set => price = value;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Ass_C_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenerateMenu();
+        }
+
+        private static List<Product> list = new List<Product>();
+
+        private static void AddProduct()
+        {
+            var product = new Product();
+            Console.WriteLine("Please enter ProductId: ");
+            product.ProductId = Console.ReadLine();
+            Console.WriteLine("Please enter ProductName:");
+            product.Name = Console.ReadLine();
+            Console.WriteLine("Please enter Price:");
+            product.Price = Console.ReadLine();
+            Console.WriteLine("Add new successfully.");
+            Console.WriteLine("Press enter to continue.");
+            Console.ReadLine();
+            list.Add(product);
+        }
+
+        private static void Display()
+        {
+            Console.WriteLine(" ---------Display product records-------------");
+            Console.WriteLine("{0, -20} {1, -20} {2,-20}", "ProductId", "ProductName", "Price");
+            Console.WriteLine("----------------------------------------------------");
+            foreach (var product in list)
+            {
+                Console.WriteLine("{0, -20} {1, -20} {2,-20}", product.ProductId, product.Name, product.Price);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Delete()
+        {
+            Console.WriteLine("Please enter ProductID");
+            Console.WriteLine("Successfully deleted.");
+            string id = Console.ReadLine();
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].ProductId.Equals(id))
+                {
+                    list.RemoveAt(i);
+                }
+            }
+        }
+
+        private static void GenerateMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine("-------------PRODUCT------------");
+                Console.WriteLine("1.Add product records .");
+                Console.WriteLine("2.Display product records .");
+                Console.WriteLine("3.Delete product by Id .");
+                Console.WriteLine("4.Exit .");
+                Console.WriteLine("-------------------------");
+                Console.WriteLine("Please enter you choice ?");
+                var choice = Int32.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Add product records : ");
+                        AddProduct();
+                        Console.WriteLine("Đã thêm thành công.");
+                        break;
+                    case 2:
+                        Console.WriteLine("Display product records :");
+                        Display();
+                        break;
+                    case 3:
+                        Delete();
+                        Console.WriteLine("Đã xóa thành công");
+                        break;
+                    case 4:
+                        Environment.Exit(1);
+                        break;
+                }
+            }
+        }
+    }
+}
